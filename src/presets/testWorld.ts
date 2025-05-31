fa7520b851e39a26d122dc32fc4e111a814e3f91
Sazykin Georgii <sazykin.georgii@gmail.com>
2025-05-31 05:01:55 +0300

    feat: add testWorld preset

---
+++ src/presets/testWorld.ts
@@ -0,0 +1,59 @@
+import WorldMap from '../core/WorldMap';
+
+export function createTestWorld(): WorldMap {
+    const worldMap = new WorldMap({
+        width: 10,
+        height: 10,
+        emptyTileData: {
+            terrain: {
+                title: 'Равнина',
+                color: '#fafffa',
+            },
+            content: null,
+        },
+    });
+
+    for (let y = 0; y < 10; y++) {
+        let content = null;
+
+        if (y === 3) {
+            content = {
+                title: 'Мост',
+                icon: '🌉',
+                description: 'Старый дряхлый мост, проходящий через реку',
+            };
+        }
+
+        worldMap.setTileData(6, y, {
+            terrain: {
+                title: 'Река',
+                color: '#fafbff',
+            },
+            content,
+        });
+    }
+
+    const trees = [
+        { x: 2, y: 2 },
+        { x: 4, y: 2 },
+        { x: 8, y: 6 },
+        { x: 9, y: 7 },
+        { x: 9, y: 8 },
+    ];
+
+    for (let tree of trees) {
+        worldMap.setTileData(tree.x, tree.y, {
+            terrain: {
+                title: 'Равнина',
+                color: '#fafffa',
+            },
+            content: {
+                title: 'Дерево',
+                icon: '🌲',
+                description: 'Обычное дерево',
+            },
+        });
+    }
+
+    return worldMap;
+}
