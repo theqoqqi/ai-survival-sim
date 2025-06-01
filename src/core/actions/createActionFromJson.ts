92a0b737730ab5f9acc2b86d5fa983553e012a96
Sazykin Georgii <sazykin.georgii@gmail.com>
2025-06-01 08:06:28 +0300

    feat: add createActionFromJson utility

---
+++ src/core/actions/createActionFromJson.ts
@@ -0,0 +1,25 @@
+import MoveAction from './MoveAction';
+import { AddInventoryItemAction } from './AddInventoryItemAction';
+import { AddItemToStackAction } from './AddItemToStackAction';
+import { RemoveInventoryItemAction } from './RemoveInventoryItemAction';
+import { SetTileFeatureAction } from './SetTileFeatureAction';
+import Action from './Action';
+
+const factories: Record<string, (json: any) => Action<any>> = {
+    'move': MoveAction.fromJson,
+    'addItem': AddInventoryItemAction.fromJson,
+    'addItemToStack': AddItemToStackAction.fromJson,
+    'removeItem': RemoveInventoryItemAction.fromJson,
+    'setTileFeature': SetTileFeatureAction.fromJson,
+};
+
+export default function createActionFromJson(json: any): Action<any> | null {
+    const actionType = json.type as string;
+    const factory = factories[actionType];
+
+    if (!factory) {
+        return null;
+    }
+
+    return factory(json);
+}
