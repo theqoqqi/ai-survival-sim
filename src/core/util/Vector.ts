497fbe5150283fc75d31d439e0fda833d36e172e
Sazykin Georgii <sazykin.georgii@gmail.com>
2025-06-02 06:25:09 +0300

    feat: add Vectors class with utility methods

--- src/core/util/Vector.ts
+++ src/core/util/Vector.ts
@@ -3,3 +3,34 @@ export interface Vector {
     readonly x: number;
     readonly y: number;
 }
+
+export class Vectors {
+
+    static equals(a: Vector, b: Vector): boolean {
+        return a.x === b.x && a.y === b.y;
+    }
+
+    static add(a: Vector, b: Vector): Vector {
+        return {
+            x: a.x + b.x,
+            y: a.y + b.y,
+        };
+    }
+
+    static subtract(a: Vector, b: Vector): Vector {
+        return {
+            x: a.x - b.x,
+            y: a.y - b.y,
+        };
+    }
+
+    static toDirection(vector: Vector): Vector {
+        const signX = vector.x > 0 ? 1 : vector.x < 0 ? -1 : 0;
+        const signY = vector.y > 0 ? 1 : vector.y < 0 ? -1 : 0;
+
+        return {
+            x: signX,
+            y: signY,
+        };
+    }
+}
