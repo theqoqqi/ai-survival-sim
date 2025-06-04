26eefb2d483f86d1a757923d9dae024616491ec3
Sazykin Georgii <sazykin.georgii@gmail.com>
2025-06-04 13:52:02 +0300

    feat: add useComponentTranslation hook

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,6 +1,7 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import { initReactI18next, useTranslation } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import React from 'react';
 
 i18n
     .use(LanguageDetector)
@@ -20,4 +21,23 @@ async function load(locale: string) {
     const jsonFile = `./locales/${locale}/translation.json`;
 
     return (await import(jsonFile)).default;
+}
+
+export function useComponentTranslation(component: any, namespace = 'translation') {
+    const { t: originalT, ...rest } = useTranslation(namespace);
+
+    const componentName =
+        typeof component === 'string'
+            ? component
+            : component.name || 'UnknownComponent';
+
+    const t = React.useCallback((key, options = {} as any) => {
+        const fullKey = key.includes('.')
+            ? key
+            : `components.${componentName}.${key}`;
+
+        return originalT(fullKey, options);
+    }, [originalT, componentName]);
+
+    return { t, ...rest };
 }
