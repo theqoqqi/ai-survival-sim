fd09c2402c7c616b31b88bfc818c226baf9d093d
Sazykin Georgii <sazykin.georgii@gmail.com>
2025-06-04 06:44:41 +0300

    refactor: extract ImportExportSection component from ImportExportPanel

--- src/components/sidebar/ImportExportPanel.tsx
+++ src/components/sidebar/ImportExportPanel.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import WorldMap from '../../core/WorldMap';
 import { DetailsPanel } from './DetailsPanel';
-import { PersistentField } from '../util/PersistentField';
 import styles from './ImportExportPanel.module.css';
 import { SaveLoadSection } from './SaveLoadSection';
+import { ImportExportSection } from './ImportExportSection';
 
 interface ImportExportPanelProps {
     worldMap: WorldMap;
@@ -16,67 +16,20 @@ export const ImportExportPanel: React.FC<ImportExportPanelProps> = ({
     onImport,
     autoImport,
 }) => {
-    const [importJson, setImportJson] = React.useState('');
     const [message, setMessage] = React.useState<string | null>(null);
-
-    const handleExport = () => {
-        setImportJson(JSON.stringify(worldMap.toJson(), null, 2));
-    };
-
-    const handleDownload = () => {
-        const json = JSON.stringify(worldMap.toJson(), null, 2);
-        const blob = new Blob([json], { type: 'application/json' });
-        const url = URL.createObjectURL(blob);
-
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'worldMap.json';
-        a.click();
-
-        URL.revokeObjectURL(url);
-        setMessage('Карта успешно экспортирована.');
-    };
-
-    const handleImport = React.useCallback(() => {
-        try {
-            const json = JSON.parse(importJson);
-            const importedMap = WorldMap.fromJson(json);
-
-            onImport(importedMap);
-
-            setMessage('Карта успешно импортирована.');
-        } catch (e) {
-            console.error('Ошибка импорта карты:', e);
-
-            setMessage('❌ Ошибка импорта карты: ' + ((e as Error).message));
-        }
-    }, [importJson, onImport]);
-
-    React.useEffect(() => {
-        if (autoImport && importJson) {
-            handleImport();
-        }
-    }, [autoImport, handleImport, importJson]);
 
     return (
         <DetailsPanel
-            header="Импорт / Экспорт карты"
+            header='Импорт / Экспорт карты'
             classNames={{ body: styles.body }}
             collapsedByDefault
         >
-            <PersistentField
-                type="textarea"
-                label="Вставьте JSON для импорта"
-                storageKey="importWorldMap"
-                value={importJson}
-                onChange={setImportJson}
+            <ImportExportSection
+                worldMap={worldMap}
+                onImport={onImport}
+                onUpdateStatus={setMessage}
+                autoImport={autoImport}
             />
-
-            <div className={styles.buttonGroup}>
-                <button onClick={handleExport}>Показать&nbsp;JSON текущей&nbsp;карты</button>
-                <button onClick={handleDownload}>Скачать JSON</button>
-                <button onClick={handleImport}>Загрузить&nbsp;карту из&nbsp;этого&nbsp;JSON</button>
-            </div>
 
             <div className={styles.separator} />
 
---
+++ src/components/sidebar/ImportExportSection.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import WorldMap from '../../core/WorldMap';
+import { PersistentField } from '../util/PersistentField';
+import styles from './ImportExportSection.module.css';
+
+interface ImportExportSectionProps {
+    worldMap: WorldMap;
+    onImport: (importedMap: WorldMap) => void;
+    onUpdateStatus: (status: string | null) => void;
+    autoImport?: boolean;
+}
+
+export const ImportExportSection: React.FC<ImportExportSectionProps> = ({
+    worldMap,
+    onImport,
+    onUpdateStatus,
+    autoImport,
+}) => {
+    const [importJson, setImportJson] = React.useState('');
+
+    const setStatus = React.useCallback((status: string) => {
+        onUpdateStatus(status);
+    }, [onUpdateStatus]);
+
+    const handleExport = () => {
+        setImportJson(JSON.stringify(worldMap.toJson(), null, 2));
+    };
+
+    const handleDownload = () => {
+        const json = JSON.stringify(worldMap.toJson(), null, 2);
+        const blob = new Blob([json], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'worldMap.json';
+        a.click();
+
+        URL.revokeObjectURL(url);
+        setStatus('Карта успешно экспортирована.');
+    };
+
+    const handleImport = React.useCallback(() => {
+        try {
+            const json = JSON.parse(importJson);
+            const importedMap = WorldMap.fromJson(json);
+
+            onImport(importedMap);
+
+            setStatus('Карта успешно импортирована.');
+        } catch (e) {
+            console.error('Ошибка импорта карты:', e);
+
+            setStatus('❌ Ошибка импорта карты: ' + ((e as Error).message));
+        }
+    }, [importJson, onImport, setStatus]);
+
+    React.useEffect(() => {
+        if (autoImport && importJson) {
+            handleImport();
+        }
+    }, [autoImport, handleImport, importJson]);
+
+    return (
+        <>
+            <PersistentField
+                type='textarea'
+                label='Вставьте JSON для импорта'
+                storageKey='importWorldMap'
+                value={importJson}
+                onChange={setImportJson}
+            />
+
+            <div className={styles.buttonGroup}>
+                <button onClick={handleExport}>Показать&nbsp;JSON текущей&nbsp;карты</button>
+                <button onClick={handleDownload}>Скачать JSON</button>
+                <button onClick={handleImport}>Загрузить&nbsp;карту из&nbsp;этого&nbsp;JSON</button>
+            </div>
+        </>
+    );
+};
